8da2c6e4f1887c8f4c2eb669771b11d06838bb80
hkim113005 <70756913+hkim113005@users.noreply.github.com>
2024-10-25 13:34:23 -0700

    Initial Commit

---
+++ Luvly/LuvlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuvlyApp.swift
+//  Luvly
+//
+//  Created by Hyungjae Kim on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct LuvlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LuvlyTests/LuvlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  LuvlyTests.swift
+//  LuvlyTests
+//
+//  Created by Hyungjae Kim on 25/10/2024.
+//
+
+import Testing
+@testable import Luvly
+
+struct LuvlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
